258fc809b7fce4f74b88958cea7e5ce779ce10f9
vnshchandaliya <programinghub733@gmail.com>
2025-11-27 00:20:07 +0530

    backend setup

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+import 'dotenv/config';
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -13,7 +13,7 @@ import bookingRoutes from "./routes/bookingRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
 
 const app = express();
-const PORT = 5000;
+const PORT =  process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
