87b76fc0b04c7c90bcd4daa8582906c7fa8cef8b
dalalanshraj <dalalanshraj@gmail.com>
2026-03-03 00:40:44 +0530

    ok

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import cors from "cors";
 import axios from "axios";
 import Property from "./models/Property.js";
